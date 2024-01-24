dc10aa159f2a141acbebf006a6a7ae60779d62c8
Oleg Kiryushin <fewgwer3@yandex.ru>
2024-01-25 01:41:58 +0300

    deploying

--- frontend/src/server_data.js
+++ frontend/src/server_data.js
@@ -1,3 +1,4 @@
-const URL = process.env.BACKEND_URL || 'http://localhost:8081';
+// const URL = process.env.BACKEND_URL || 'http://localhost:8081';
+const URL = 'https://portfolio-okir-backend.onrender.com';
 
 export default URL;
