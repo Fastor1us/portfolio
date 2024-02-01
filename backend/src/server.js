de64297dbba2b17a5f408bc0d5bb5f899a83621d
Oleg Kiryushin <fewgwer3@yandex.ru>
2024-02-01 19:07:53 +0300

    v0.3.8

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { projectRoutes } from './routes/projectsRoutes.js';
 import { imagesRouter } from './routes/imagesRouter.js';
 const CLIENT_URL = process.env.CLIENT_URL || 'http://localhost:8080';
-const EXTRA_EXTERNAL_URL = process.env.EXTRA_EXTERNAL_ACCESS_HEADER || null;
+const EXTRA_EXTERNAL_URL = process.env.EXTRA_EXTERNAL_ACCESS_ORIGIN || null;
 
 const app = express();
 const PORT = process.env.PORT || 8081;
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 8081;
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', CLIENT_URL);
   if (EXTRA_EXTERNAL_URL) {
-    res.setHeader('Access-Control-Allow-Headers', EXTRA_EXTERNAL_URL);
+    res.setHeader('Access-Control-Allow-Origin', EXTRA_EXTERNAL_URL);
   }
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
